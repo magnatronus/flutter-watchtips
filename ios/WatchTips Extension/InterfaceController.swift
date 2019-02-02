e1c40655c23a507b61b72783d8ed8fdb3c4681ad
SpiralArm Consulting Ltd <projects@spiralarm.uk>
2019-02-02 12:31:27 +0000

    update watch app UI

--- ios/WatchTips Extension/InterfaceController.swift
+++ ios/WatchTips Extension/InterfaceController.swift
@@ -11,17 +11,16 @@ import Foundation
 import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController, WCSessionDelegate, WKCrownDelegate {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
     
     var tipAmount: Int = 10
     var splitBetween: Int = 1
     var billTotal = Double(0)
     var focusButton: Int = 0
-    var accumulatedCrownDelta = 0.0
 
-    @IBOutlet weak var currentSplit: WKInterfaceButton!
-    @IBOutlet weak var currentTip: WKInterfaceButton!
+    @IBOutlet var currentSplit: WKInterfaceLabel!
+    @IBOutlet var currentTip: WKInterfaceLabel!
     @IBOutlet weak var billButton: WKInterfaceButton!
     @IBOutlet weak var currentTotal: WKInterfaceLabel!
     @IBOutlet weak var costEach: WKInterfaceLabel!
@@ -33,59 +32,9 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate, WKCrownDele
     }
     
     
-    
-    func crownDidBecomeIdle(_ crownSequencer: WKCrownSequencer?) {
-        accumulatedCrownDelta = 0.0
-    }
-    
-    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
-        accumulatedCrownDelta += rotationalDelta
-        let threshold = 0.05
-        
-        // do nothing if delta is less that threshold
-        guard abs(accumulatedCrownDelta) > threshold else {
-            return
-        }
-        
-        if focusButton == 1 {
-            if accumulatedCrownDelta > 0 {
-                splitBetween += 1
-            } else {
-                splitBetween -= 1
-            }
-            if splitBetween < 1 {
-                splitBetween = 1
-            }
-            if splitBetween > 50 {
-                splitBetween = 50
-            }
-        }
-        
-        if focusButton == 2 {
-            if accumulatedCrownDelta > 0 {
-                tipAmount += 1
-            } else {
-                tipAmount -= 1
-            }
-            if tipAmount < 0 {
-                tipAmount = 0
-            }
-            if tipAmount > 100 {
-                tipAmount = 100
-            }
-        }
-        
-        accumulatedCrownDelta = 0
-        updateCalc()
-        
-    }
-    
-    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
-        crownSequencer.delegate = self
         updateCalc()
     }
     
@@ -97,7 +46,6 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate, WKCrownDele
             session.delegate = self
             session.activate()
         }
-        crownSequencer.focus()
         updateCalc()
     }
     
@@ -106,36 +54,38 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate, WKCrownDele
         super.didDeactivate()
     }
     
-    
-    // chnage how many the bill is split between
-    @IBAction func showSplitController() {
-        resetButtonFocus()
-        currentSplit?.setBackgroundColor(UIColor(red: 0/255, green: 128/255, blue: 255/255, alpha: 1.0))
-        focusButton = 1
-        //presentController(withName: "SplitController", context: self)
+    @IBAction func splitDecrease() {
+        if(splitBetween > 1){
+            splitBetween -= 1
+        }
+        updateCalc()
     }
     
-    
-    @IBAction func showTipController() {
-        resetButtonFocus()
-        currentTip?.setBackgroundColor(UIColor(red: 0/255, green: 128/255, blue: 255/255, alpha: 1.0))
-        focusButton = 2
-        //presentController(withName: "TipController", context: self)
+    @IBAction func splitIncrease() {
+        if(splitBetween < 99){
+            splitBetween += 1
+        }
+        updateCalc()
     }
     
+    @IBAction func tipPercentReduce() {
+        if(tipAmount>0){
+            tipAmount -= 1
+        }
+        updateCalc()
+    }
+    
+    @IBAction func tipPercentIncrease() {
+        if(tipAmount<100){
+            tipAmount += 1
+        }
+        updateCalc()
+    }
     
     @IBAction func showBillController() {
-        resetButtonFocus()
         presentController(withName: "BillController", context: self)
     }
     
-    // reset the focus to the currently selected button
-    func resetButtonFocus(){
-        focusButton = 0
-        currentSplit?.setBackgroundColor(UIColor.darkGray)
-        currentTip?.setBackgroundColor(UIColor.darkGray)
-        billButton?.setBackgroundColor(UIColor.darkGray)
-    }
     
     // Update the actual calculations
     func updateCalc(){
@@ -146,8 +96,8 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate, WKCrownDele
         let perPerson = billWithTip / Double(splitBetween)
         
         // update display
-        currentTip.setTitle("\(tipAmount)%")
-        currentSplit.setTitle("\(splitBetween)")
+        currentTip.setText("\(tipAmount)%")
+        currentSplit.setText("\(splitBetween)")
         currentTotal.setText(String(format: "%.2f", billTotal))
         withTip.setText(String(format: "%.2f", billWithTip))
         costEach.setText(String(format: "%.2f", perPerson))
